fd60cb4845c6c94c6e77ea648c2bbd30adc5d5ff
Alireza Tavakoli <43365143+mirza-developer@users.noreply.github.com>
2025-01-16 23:18:56 +0330

    add sass for monitor width #43

--- Avhrm.UI.Shared/wwwroot/scripts/Site.js
+++ Avhrm.UI.Shared/wwwroot/scripts/Site.js
@@ -1,1 +1,12 @@
-//# sourceMappingURL=Site.js.map
+function checkMediaQueries(dotNetHelper, mediaQueries) {
+    function onMediaQueryChange(event, name) {
+        dotNetHelper.invokeMethodAsync('OnMediaQueryChanged', name, event.matches);
+    }
+
+    mediaQueries.forEach(mq => {
+        const mediaQueryList = window.matchMedia(mq.query);
+        mediaQueryList.addEventListener('change', event => onMediaQueryChange(event, mq.name));
+        
+        onMediaQueryChange(mediaQueryList, mq.name);
+    });
+}
